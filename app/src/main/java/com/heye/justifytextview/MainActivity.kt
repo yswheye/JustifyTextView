8c0a4aea11d066c023dd3b1b6351808e496f052a
heye <yswheye@gmail.com>
2021-11-25 15:52:17 +0800

    commit first

---
+++ app/src/main/java/com/heye/justifytextview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.heye.justifytextview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/heye/view/JustifyTextView.java
@@ -0,0 +1,120 @@
+package com.heye.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class JustifyTextView extends AppCompatTextView {
+    private float mLineY;
+    private int mViewWidth;
+    private float lineHeight;
+    private TextPaint mPaint;
+    private int lineCount;
+    private boolean maxLineLimit;
+
+    public JustifyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        Layout layout = getLayout();
+        mPaint = getPaint();
+        mPaint.setTextSize(getTextSize());
+        mPaint.setColor(getCurrentTextColor());
+        mPaint.drawableState = getDrawableState();
+        Paint.FontMetrics fm = mPaint.getFontMetrics();
+        int textHeight = (int) (Math.ceil(fm.descent - fm.ascent));
+        lineHeight = (int) (textHeight * layout.getSpacingMultiplier() + layout.getSpacingAdd());
+        lineCount = layout.getLineCount();
+        //设置过maxLines属性
+        if (maxLineLimit = getMaxLines() != Integer.MAX_VALUE) {
+            lineCount = getMaxLines() - 1;
+            setMaxLines(getMaxLines() + 1);
+        }
+        int viewHeight = (int) (lineHeight * lineCount - Math.ceil(fm.bottom - fm.leading));
+        setMeasuredDimension(getMeasuredWidth(), viewHeight);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mViewWidth = getMeasuredWidth();
+        String text = getText().toString();
+        mLineY = getTextSize();
+        Layout layout = getLayout();
+
+        // layout.getLayout()在4.4.3出现NullPointerException
+        if (layout == null) {
+            return;
+        }
+
+        for (int i = 0; i < lineCount; i++) {
+            int lineStart = layout.getLineStart(i);
+            int lineEnd = layout.getLineEnd(i);
+            float width = StaticLayout.getDesiredWidth(text, lineStart, lineEnd, getPaint());
+            String line = text.substring(lineStart, lineEnd);
+            //处理ellipsize end ...
+            if (maxLineLimit) {
+                if (i == lineCount - 1 && line.length() > 2) {
+                    line = line.substring(0, line.length() - 1) + "...";
+                }
+            }
+            if (needScale(line) && i < lineCount - 1) {
+                drawScaledText(canvas, line, width);
+            } else {
+                canvas.drawText(line, 0, mLineY, mPaint);
+            }
+            mLineY += lineHeight;
+        }
+    }
+
+    private void drawScaledText(Canvas canvas, String line, float lineWidth) {
+        float x = 0;
+        if (isFirstLineOfParagraph(line)) {
+            String blanks = "  ";
+            canvas.drawText(blanks, x, mLineY, getPaint());
+            float bw = StaticLayout.getDesiredWidth(blanks, getPaint());
+            x += bw;
+
+            line = line.substring(3);
+        }
+
+        int gapCount = line.length() - 1;
+        int i = 0;
+        if (line.length() > 2 && line.charAt(0) == 12288 && line.charAt(1) == 12288) {
+            String substring = line.substring(0, 2);
+            float cw = StaticLayout.getDesiredWidth(substring, getPaint());
+            canvas.drawText(substring, x, mLineY, getPaint());
+            x += cw;
+            i += 2;
+        }
+
+        float d = (mViewWidth - lineWidth) / gapCount;
+        for (; i < line.length(); i++) {
+            String c = String.valueOf(line.charAt(i));
+            float cw = StaticLayout.getDesiredWidth(c, getPaint());
+            canvas.drawText(c, x, mLineY, getPaint());
+            x += cw + d;
+        }
+    }
+
+    private boolean isFirstLineOfParagraph(String line) {
+        return line.length() > 3 && line.charAt(0) == ' ' && line.charAt(1) == ' ';
+    }
+
+    private boolean needScale(String line) {
+        if (line == null || line.length() == 0) {
+            return false;
+        } else {
+            return line.charAt(line.length() - 1) != '\n';
+        }
+    }
+
+}
